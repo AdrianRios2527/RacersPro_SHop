65bb7ad6735671461633f034e8c4cfd00e7ae8e0
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2023-12-26 14:29:38 +0000

    Mejoras in header

--- src/js/component/header.jsx
+++ src/js/component/header.jsx
@@ -13,7 +13,7 @@ export const Header = () => {
   return (
     <div className="">
       <div className="p-3 text-center">
-        <h1 >RaceSPro</h1>
+        <h1 id="tittleHeader">RaceSPro</h1>
         <span>Ready to rock and roll!</span>
       </div>
       <div className="text-center w-100">
